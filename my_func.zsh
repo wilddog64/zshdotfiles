4ada191d0e0d9417b2d205ecfb63bd37b52270a7
chengkai liang <ckm.liang@gmail.com>
2015-04-06 09:14:16 -0700

    add function to check if current context is at master branch

--- my_func.zsh
+++ my_func.zsh
@@ -15,10 +15,19 @@ function _is_this_a_git_repo() {
     fi
 }
 
+function _at_master_branch() {
+    git branch | grep '\* master'
+    return $?
+}
+
 # kill_git_branch: is a bash function that will delete a merged branch base on master
 kill_git_branch() {
 
     _is_this_a_git_repo
+    if [[ $(_at_master_branch) > 0 ]]; then
+        echo current context is not at master branch, abort!
+        kill -INT $$
+    fi
     git branch --merged | grep -v master | xargs git branch -d 2> /dev/null
 
 }
