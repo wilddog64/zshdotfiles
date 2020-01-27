74e2418c0d25336415b47870c126b708d5af8ee8
CK <chengkai.liang@bigfishgames.com>
2020-01-27 11:02:17 -0800

    a shell function to select a selected session

--- my_func.zsh
+++ my_func.zsh
@@ -29,3 +29,13 @@ tm() {
   fi
   session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
 }
+
+# fs [FUZZY PATTERN] - Select selected tmux session
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fs() {
+  local session
+  session=$(tmux list-sessions -F "#{session_name}" | \
+    fzf --query="$1" --select-1 --exit-0) &&
+  tmux switch-client -t "$session"
+}
