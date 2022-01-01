03658e85e88f257fd7571133fd07783a411fae6b
chengkai liang <chengkli@cisco.com>
2022-01-01 14:31:36 -0800

    remove zsh-vi-mode plugin

--- zplugs.zsh
+++ zplugs.zsh
@@ -1,4 +1,3 @@
-zplug "jeffreytse/zsh-vi-mode", as:command
 zplug "theme/agnoster", from:oh-my-zsh, as:theme
 zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/per-directory-history", from:oh-my-zsh
