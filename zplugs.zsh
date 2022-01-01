61dd48ce985eb0186efd2c56db61411afb8f805a
chengkai liang <chengkli@cisco.com>
2022-01-01 12:58:15 -0800

    use zplug to manage per-directory-history plugin
    use zplug to manage zsh plugins

--- zplugs.zsh
+++ zplugs.zsh
@@ -1,3 +1,14 @@
 zplug "jeffreytse/zsh-vi-mode", as:command
 zplug "theme/agnoster", from:oh-my-zsh, as:theme
 zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/per-directory-history", from:oh-my-zsh
+zplug "plugins/z", from:oh-my-zsh
+zplug "plugins/aws", from:oh-my-zsh
+zplug "plugins/brew", from:oh-my-zsh
+zplug "plugins/docker", from:oh-my-zsh
+zplug "plugins/terraform", from:oh-my-zsh
+zplug "plugins/dotnet", from:oh-my-zsh
+zplug "plugins/gh", from:oh-my-zsh
+zplug "plugins/kops", from:oh-my-zsh
+zplug "plugins/vagrant", from:oh-my-zsh
+zplug "plugins/terraform", from:oh-my-zsh
