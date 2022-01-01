032b1beb8d4c397d4633c782d177766896696650
chengkai liang <chengkli@cisco.com>
2022-01-01 12:55:13 -0800

    use zplug to manage git plugin

--- zplugs.zsh
+++ zplugs.zsh
@@ -1,4 +1,3 @@
 zplug "jeffreytse/zsh-vi-mode", as:command
-zplug "zsh-users/zsh-autosuggestions.git"
-zplug "zsh-users/zsh-syntax-highlighting.git"
 zplug "theme/agnoster", from:oh-my-zsh, as:theme
+zplug "plugins/git", from:oh-my-zsh
