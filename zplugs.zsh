bea8864cf2725f9ebaab7c5774dc227bf66edb44
chengkai liang <chengkli@cisco.com>
2022-01-02 07:58:15 -0800

    add keychain plugin

--- zplugs.zsh
+++ zplugs.zsh
@@ -14,3 +14,4 @@ zplug "plugins/terraform", from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions", defer:2
 zplug "zsh-users/vi-mode", from:oh-my-zsh
+zplug "mogensen/keychain", as:command
