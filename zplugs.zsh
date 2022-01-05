c4ee0ecaa4eeb07de4fa954a07eb3fccbb8b10ce
chengkai liang <chengkli@cisco.com>
2022-01-04 19:31:42 -0800

    enable vi-mode and visual mode

--- zplugs.zsh
+++ zplugs.zsh
@@ -13,8 +13,9 @@ zplug "plugins/vagrant", from:oh-my-zsh
 zplug "plugins/terraform", from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions", defer:2
-# zplug "zsh-users/vi-mode", from:oh-my-zsh
+zplug "zsh-users/vi-mode", from:oh-my-zsh
 zplug "mogensen/keychain", as:command, hook-build:"ln -sf $ZPLUG_REPOS/mogensen/keychain/keychain.sh $ZPLUG_BIN/keychain.sh"
 zplug "lib/clipboard", from:oh-my-zsh
 zplug "Jxck/dotfiles", as:command, use:"bin/{histuniq,color}"
 zplug "junegunn/fzf", as:command, hook-build:"make install", use:"bin/fzf"
+zplug "b4b4r07/zsh-vimode-visual", defer:3
