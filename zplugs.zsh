d815a9cb8561523635188ea015da2fbed4fe4e04
chengkai liang <chengkli@cisco.com>
2022-01-23 09:23:20 -0800

    remove fzf

--- zplugs.zsh
+++ zplugs.zsh
@@ -17,7 +17,6 @@ zplug "zsh-users/vi-mode", from:oh-my-zsh
 zplug "mogensen/keychain", as:command, hook-build:"ln -sf $ZPLUG_REPOS/mogensen/keychain/keychain.sh $ZPLUG_BIN/keychain.sh"
 zplug "lib/clipboard", from:oh-my-zsh
 zplug "Jxck/dotfiles", as:command, use:"bin/{histuniq,color}"
-zplug "junegunn/fzf", as:command, hook-build:"make install", use:"bin/fzf"
 zplug "b4b4r07/zsh-vimode-visual", defer:3
 zplug "lib/directories", from:oh-my-zsh
 zplug "greymd/tmux-xpanes", as:command, use:"bin/xpanes"
