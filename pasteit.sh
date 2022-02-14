bfb82e32433b686ddfeffb45ef70c69cc1b01cbe
chengkai liang <chengkli@cisco.com>
2022-02-14 06:10:22 -0800

    remove trailing whitespaces

--- pasteit.sh
+++ pasteit.sh
@@ -2,25 +2,25 @@
 emulate -L zsh
 
 if [[ "${OSTYPE}" == darwin* ]] && (( ${+commands[pbcopy]} )) && (( ${+commands[pbpaste]} )); then
-   pbpaste; 
+   pbpaste;
 elif [[ "${OSTYPE}" == (cygwin|msys)* ]]; then
-   cat /dev/clipboard; 
+   cat /dev/clipboard;
 elif [ -n "${WAYLAND_DISPLAY:-}" ] && (( ${+commands[wl-copy]} )) && (( ${+commands[wl-paste]} )); then
-   wl-paste; 
+   wl-paste;
 elif [ -n "${DISPLAY:-}" ] && (( ${+commands[xclip]} )); then
-   xclip -out -selection clipboard; 
+   xclip -out -selection clipboard;
 elif [ -n "${DISPLAY:-}" ] && (( ${+commands[xsel]} )); then
-   xsel --clipboard --output; 
+   xsel --clipboard --output;
 elif (( ${+commands[lemonade]} )); then
-   lemonade paste; 
+   lemonade paste;
 elif (( ${+commands[doitclient]} )); then
-   doitclient wclip -r; 
+   doitclient wclip -r;
 elif (( ${+commands[win32yank]} )); then
-   win32yank -o; 
+   win32yank -o;
 elif [[ $OSTYPE == linux-android* ]] && (( $+commands[termux-clipboard-set] )); then
-   termux-clipboard-get; 
+   termux-clipboard-get;
 elif [ -n "${TMUX:-}" ] && (( ${+commands[tmux]} )); then
-   tmux save-buffer -; 
+   tmux save-buffer -;
 elif [[ $(uname -r) = *icrosoft* ]]; then
-   powershell.exe -noprofile -command Get-Clipboard; 
+   powershell.exe -noprofile -command Get-Clipboard;
 fi
