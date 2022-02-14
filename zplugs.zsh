6391eaa33f24e34e67a6b3c04f56b9badf9444aa
chengkai liang <chengkli@cisco.com>
2022-02-13 18:15:07 -0800

    remove brew and fzf plugins

--- zplugs.zsh
+++ zplugs.zsh
@@ -3,7 +3,6 @@ zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/per-directory-history", from:oh-my-zsh
 zplug "plugins/z", from:oh-my-zsh
 zplug "plugins/aws", from:oh-my-zsh
-zplug "plugins/brew", from:oh-my-zsh
 zplug "plugins/docker", from:oh-my-zsh
 zplug "plugins/terraform", from:oh-my-zsh
 zplug "plugins/dotnet", from:oh-my-zsh
@@ -17,7 +16,6 @@ zplug "zsh-users/vi-mode", from:oh-my-zsh
 zplug "mogensen/keychain", as:command, hook-build:"ln -sf $ZPLUG_REPOS/mogensen/keychain/keychain.sh $ZPLUG_BIN/keychain.sh"
 zplug "lib/clipboard", from:oh-my-zsh
 zplug "Jxck/dotfiles", as:command, use:"bin/{histuniq,color}"
-zplug "junegunn/fzf", as:command, hook-build:"make install", use:"bin/fzf"
 zplug "b4b4r07/zsh-vimode-visual", defer:3
 zplug "lib/directories", from:oh-my-zsh
 zplug "greymd/tmux-xpanes", as:command, use:"bin/xpanes"
