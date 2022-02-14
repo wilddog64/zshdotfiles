902e1a8a439561f0adf6293cadd9d9b07f69f74c
chengkai liang <chengkli@cisco.com>
2022-02-13 18:15:33 -0800

    remove keychain plugin

--- zplugs.zsh
+++ zplugs.zsh
@@ -13,7 +13,6 @@ zplug "plugins/terraform", from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions", defer:2
 zplug "zsh-users/vi-mode", from:oh-my-zsh
-zplug "mogensen/keychain", as:command, hook-build:"ln -sf $ZPLUG_REPOS/mogensen/keychain/keychain.sh $ZPLUG_BIN/keychain.sh"
 zplug "lib/clipboard", from:oh-my-zsh
 zplug "Jxck/dotfiles", as:command, use:"bin/{histuniq,color}"
 zplug "b4b4r07/zsh-vimode-visual", defer:3
