9d1b5fbaa27887f185a8e1d0d658caf73ad80058
chengkai liang <chengkli@cisco.com>
2022-03-20 16:06:24 -0700

    add git-open plugin

--- zplugs.zsh
+++ zplugs.zsh
@@ -24,3 +24,4 @@ zplug "greymd/tmux-xpanes", as:command, use:"bin/xpanes"
 zplug "cxreg/smartcd", from:github, hook-build: "make install; ln -sf ~/.zsh/smartcd_config ~/.smartcd_config"
 zplug "pyenv/pyenv", from:github, hook-build: "ln -sf $ZPLUG_REPOS/pyenv/pyenv ~/.pyenv"
 zplug "wfxr/forgit", from:github, as:command, hook-build: "ln -sf $ZPLUG_REPOS/wfxr/forgit/bin/git-forgit $ZPLUG_HOME/bin"
+zplug "paulirish/git-open", as:plugin
