ce358e0e1ec27e5b7b045f510633bf71dcb5fe93
chengkai liang <chengkli@cisco.com>
2023-01-09 15:47:08 -0800

    script to find aws vpc dependencies

---
+++ find_aws_vpc_deps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+vpc="vpc-048b9a080bba63db4"
+aws ec2 describe-internet-gateways --filters 'Name=attachment.vpc-id,Values='$vpc | grep InternetGatewayId
+aws ec2 describe-subnets --filters 'Name=vpc-id,Values='$vpc | grep SubnetId
+aws ec2 describe-route-tables --filters 'Name=vpc-id,Values='$vpc | grep RouteTableId
+aws ec2 describe-network-acls --filters 'Name=vpc-id,Values='$vpc | grep NetworkAclId
+aws ec2 describe-vpc-peering-connections --filters 'Name=requester-vpc-info.vpc-id,Values='$vpc | grep VpcPeeringConnectionId
+aws ec2 describe-vpc-endpoints --filters 'Name=vpc-id,Values='$vpc | grep VpcEndpointId
+aws ec2 describe-nat-gateways --filter 'Name=vpc-id,Values='$vpc | grep NatGatewayId
+aws ec2 describe-security-groups --filters 'Name=vpc-id,Values='$vpc | grep GroupId
+aws ec2 describe-instances --filters 'Name=vpc-id,Values='$vpc | grep InstanceId
+aws ec2 describe-vpn-connections --filters 'Name=vpc-id,Values='$vpc | grep VpnConnectionId
+aws ec2 describe-vpn-gateways --filters 'Name=attachment.vpc-id,Values='$vpc | grep VpnGatewayId
+aws ec2 describe-network-interfaces --filters 'Name=vpc-id,Values='$vpc | grep NetworkInterfaceId
+aws ec2 describe-carrier-gateways --filters Name=vpc-id,Values=$vpc | grep CarrierGatewayId
+aws ec2 describe-local-gateway-route-table-vpc-associations --filters "Name=vpc-id,Values=$vpc"
