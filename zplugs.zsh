b30b36dfabf56fa86d57f0878f047709741613e1
chengkai liang <chengkai.liang@costcotravel.com>
2023-04-16 21:34:07 -0700

    disable aws plugin

--- zplugs.zsh
+++ zplugs.zsh
@@ -2,7 +2,7 @@ zplug "theme/agnoster", from:oh-my-zsh, as:theme
 zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/per-directory-history", from:oh-my-zsh
 zplug "plugins/z", from:oh-my-zsh
-zplug "plugins/aws", from:oh-my-zsh
+# zplug "plugins/aws", from:oh-my-zsh
 zplug "plugins/brew", from:oh-my-zsh
 zplug "plugins/docker", from:oh-my-zsh
 zplug "plugins/terraform", from:oh-my-zsh
