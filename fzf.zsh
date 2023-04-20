6d5443a67d5a067ba3ec883f798b6632ac216b61
chengkai liang <chengkai.liang@costcotravel.com>
2023-04-20 14:35:16 -0700

    update fzf autocomplete scripts

--- fzf.zsh
+++ fzf.zsh
@@ -1,13 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$HOMEBREW_PREFIX/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$HOMEBREW_PREFIX/opt/fzf/bin"
-fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
+# if [[ ! "$PATH" == *$HOMEBREW_PREFIX/opt/fzf/bin* ]]; then
+#   export PATH="${PATH:+${PATH}:}$HOMEBREW_PREFIX/opt/fzf/bin"
+# fi
+#
+# # Auto-completion
+# # ---------------
+# [[ $- == *i* ]] && source "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh" 2> /dev/null
+#
+# # Key bindings
+# # ------------
+# source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
+source ~/.zsh/fzf-shell/key-bindings.zsh
