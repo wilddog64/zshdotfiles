d9daea3e1d9b37ca4d98474472e2f1b294c3d7cd
chengkai liang <chengkai.liang@costcotravel.com>
2023-05-09 12:36:12 -0700

    load kubectl plugin

--- zplugs.zsh
+++ zplugs.zsh
@@ -12,6 +12,7 @@ zplug "plugins/kops", from:oh-my-zsh
 zplug "plugins/vagrant", from:oh-my-zsh
 zplug "plugins/terraform", from:oh-my-zsh
 zplug "plugins/kitchen", from:oh-my-zsh
+zplug "plugins/kubectl", from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions", defer:2
 zplug "zsh-users/vi-mode", from:oh-my-zsh
