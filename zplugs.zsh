6dd38a19567e62cd1676a1c426c574fe907c27d4
chengkai liang <chengkai.liang@costcotravel.com>
2023-05-13 19:13:55 -0700

    kubectl plugin

--- zplugs.zsh
+++ zplugs.zsh
@@ -15,6 +15,7 @@ zplug "plugins/kitchen", from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions", defer:2
 zplug "zsh-users/vi-mode", from:oh-my-zsh
+zplug "zsh-users/kubectl", from:oh-my-zsh
 zplug "mogensen/keychain", as:command, hook-build:"ln -sf $ZPLUG_REPOS/mogensen/keychain/keychain.sh $ZPLUG_BIN/keychain.sh"
 zplug "lib/clipboard", from:oh-my-zsh
 zplug "Jxck/dotfiles", as:command, use:"bin/{histuniq,color}"
