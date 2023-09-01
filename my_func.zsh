d5aad7c819258a27918620ce5a0277f50be9000c
chengkai liang <chengkai.liang@costcotravel.com>
2023-09-01 08:30:19 -0700

    a function istior to report if any k8s namespace has istio installed and configured

--- my_func.zsh
+++ my_func.zsh
@@ -171,30 +171,3 @@ s3rm() {
 istior() {
    k api-resources| grep -i istio | awk '{ print $4 }' | grep -v true | while read cr; do k get $(echo $cr | tr '[:upper:]' '[:lower:]') --all-namespaces; done
 }
-
-enable_k8s_istio() {
-   namespace=$1
-   kubectl label namespace $namespace istio-injection=enabled
-}
-
-analyze_istio() {
-   namespace=$1
-   istioctl -n $namespace analyze
-}
-
-list_istio_secrets() {
-   kubectl -n istio-system get secrets
-}
-
-get_istio_secrets() {
-   kubectl -n istio-system get secrets $1 -o yaml
-}
-
-describe_istio_secret() {
-   kubectl -n istio-system describe secrets $1
-}
-
-vim() {
-   mvim=$(brew --prefix macvim)/bin/vi
-   $mvim "$@"
-}
