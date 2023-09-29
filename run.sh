85aeda338092a5cc0d2e4e6feeae67f283e2a8a1
chengkai liang <chengkai.liang@costcotravel.com>
2023-09-29 08:33:33 -0700

    configure powershell profile and share location

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 podman run -v $(pwd):/mnt/workspace -w /mnt/workspace \
+           -v $HOME/src/gitrepo/costco/build-deploy/config:/root/.config \
+           -v $HOME/src/gitrepo/costco/build-deploy/local:/root/.local \
            -e PACIFIC_JENKINS_URL="${PACIFIC_JENKINS_URL}" \
            -e PACIFIC_JENKINS_USER=${PACIFIC_JENKINS_USER} \
            -e PACIFIC_JENKINS_PASS=${PACIFIC_JENKINS_PASS} \
