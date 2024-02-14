70517d756f5fb73809c71ddac5ac8083b18e3bae
chengkai liang <chengkai.liang@costcotravel.com>
2024-02-14 07:50:46 -0800

    add zsh-autopair plugin

--- zplugs.zsh
+++ zplugs.zsh
@@ -2,6 +2,7 @@ zplug "theme/agnoster", from:oh-my-zsh, as:theme
 zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/per-directory-history", from:oh-my-zsh
 zplug "plugins/z", from:oh-my-zsh
+zplug "plugins/aws", from:oh-my-zsh
 zplug "plugins/brew", from:oh-my-zsh
 zplug "plugins/docker", from:oh-my-zsh
 zplug "plugins/terraform", from:oh-my-zsh
@@ -9,6 +10,7 @@ zplug "plugins/dotnet", from:oh-my-zsh
 zplug "plugins/gh", from:oh-my-zsh
 zplug "plugins/kops", from:oh-my-zsh
 zplug "plugins/vagrant", from:oh-my-zsh
+zplug "plugins/terraform", from:oh-my-zsh
 zplug "plugins/kitchen", from:oh-my-zsh
 zplug "plugins/kubectl", from:oh-my-zsh
 zplug "plugins/helm", from:oh-my-zsh
@@ -16,7 +18,6 @@ zplug "plugins/azure", from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions", defer:2
 zplug "zsh-users/vi-mode", from:oh-my-zsh
-zplug "zsh-users/kubectl", from:oh-my-zsh, as:plugin
 zplug "mogensen/keychain", as:command, hook-build:"ln -sf $ZPLUG_REPOS/mogensen/keychain/keychain.sh $ZPLUG_BIN/keychain.sh"
 zplug "lib/clipboard", from:oh-my-zsh
 zplug "b4b4r07/zsh-vimode-visual", defer:3
@@ -28,4 +29,5 @@ zplug "wfxr/forgit", from:github, as:command, hook-build: "ln -sf $ZPLUG_REPOS/w
 zplug "paulirish/git-open", as:plugin
 zplug 'mfaerevaag/wd', as:plugin
 zplug 'hlissner/zsh-autopair', as:plugin
+# zplug 'jeffreytse/zsh-vi-mode', as:plugin
 zplug romkatv/powerlevel10k, as:theme, depth:1
