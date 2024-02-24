299fcf8810e251a352a5263e5338b66f8a0da098
chengkai liang <chengkai.liang@costcotravel.com>
2024-02-24 12:27:50 -0800

    powershell integration with windowns terminal

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -35,3 +35,54 @@ Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 
 # enable direnv for powershell
 Invoke-Expression "$(direnv hook pwsh)"
+
+# powershell terminal integration
+$Global:__LastHistoryId = -1
+
+function Global:__Terminal-Get-LastExitCode {
+  if ($? -eq $True) {
+    return 0
+  }
+  $LastHistoryEntry = $(Get-History -Count 1)
+  $IsPowerShellError = $Error[0].InvocationInfo.HistoryId -eq $LastHistoryEntry.Id
+  if ($IsPowerShellError) {
+    return -1
+  }
+  return $LastExitCode
+}
+
+function prompt {
+
+  # First, emit a mark for the _end_ of the previous command.
+
+  $gle = $(__Terminal-Get-LastExitCode);
+  $LastHistoryEntry = $(Get-History -Count 1)
+  # Skip finishing the command if the first command has not yet started
+  if ($Global:__LastHistoryId -ne -1) {
+    if ($LastHistoryEntry.Id -eq $Global:__LastHistoryId) {
+      # Don't provide a command line or exit code if there was no history entry (eg. ctrl+c, enter on no command)
+      $out += "`e]133;D`a"
+    } else {
+      $out += "`e]133;D;$gle`a"
+    }
+  }
+
+
+  $loc = $($executionContext.SessionState.Path.CurrentLocation);
+
+  # Prompt started
+  $out += "`e]133;A$([char]07)";
+
+  # CWD
+  $out += "`e]9;9;`"$loc`"$([char]07)";
+
+  # (your prompt here)
+  $out += "PWSH $loc$('>' * ($nestedPromptLevel + 1)) ";
+
+  # Prompt ended, Command started
+  $out += "`e]133;B$([char]07)";
+
+  $Global:__LastHistoryId = $LastHistoryEntry.Id
+
+  return $out
+}
