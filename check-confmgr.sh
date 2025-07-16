d5e0e631637a22f5862ed784e4cd84ef59a7600e
chengkai liang <chengkai.liang@costcotravel.com>
2025-07-16 14:55:32 -0700

    make check-confmgr help message more clear

--- check-confmgr.sh
+++ check-confmgr.sh
@@ -5,7 +5,7 @@ env_name=$2
 container=$3
 if [ -z "$service_name" ] || [ -z "$env_name" ] || [ -z "$container" ]; then
    echo "Usage: $0 <service_name> <env_name> <container>"
-   echo "Example: $0 config-manager-service qa2 istio-proxy"
+   echo "Example: $0 config-manager-service qa2 odysseus-sailing-data-767886ff78-gp7qr"
    exit 1
 fi
 
