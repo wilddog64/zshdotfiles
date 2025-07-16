9a5079f3934b759d2cbef2b7605b9636718d86dd
chengkai liang <chengkai.liang@costcotravel.com>
2025-07-16 14:52:10 -0700

    a shell script to check configmgr from within a k8s container via istio-proxy

---
+++ check-confmgr.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+service_name=$1
+env_name=$2
+container=$3
+if [ -z "$service_name" ] || [ -z "$env_name" ] || [ -z "$container" ]; then
+   echo "Usage: $0 <service_name> <env_name> <container>"
+   echo "Example: $0 config-manager-service qa2 istio-proxy"
+   exit 1
+fi
+
+kubectl exec -n "$service_name-$env_name" "$container" -c istio-proxy -- curl -kv https://config-manager-service-qa2.k8s.qa.costcotravel.com/${service_name}/${env_name}
+
