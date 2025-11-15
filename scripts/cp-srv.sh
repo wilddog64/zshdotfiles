e80e5bf7e9c3ef43f611606fc88a3ed1ca87d3d2
chengkai liang <ckm.liang@gmail.com>
2025-11-15 15:30:57 -0800

    cp-srv.sh: add autossh

--- scripts/cp-srv.sh
+++ scripts/cp-srv.sh
@@ -5,3 +5,9 @@ nohup socat TCP-LISTEN:7365,bind=127.0.0.1,fork SYSTEM:"pbpaste" \
 # Accept data and copy to Mac clipboard (pbcopy) on 127.0.0.1:7366
 nohup socat TCP-LISTEN:7366,bind=127.0.0.1,fork SYSTEM:"pbcopy" \
   >/tmp/paste.log 2>&1 &  echo $! > ~/.cache/paste808.pid
+
+AUTOSSH_GATETIME=0 AUTOSSH_LOGFILE=~/.autossh-ubuntu.log \
+autossh -M 0 -N -f ubuntu \
+  -o ExitOnForwardFailure=yes \
+  -R 127.0.0.1:7365:127.0.0.1:7365 \
+  -R 127.0.0.1:7366:127.0.0.1:7366
